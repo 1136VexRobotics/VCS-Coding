c909737f850c7f7cc22ce8377af4be4f5284a374
1136-Robotics <45320637+1136-Robotics@users.noreply.github.com>
2018-11-24 15:06:02 -0800

    Delete robot-config.h

--- robot-config.h
+++
@@ -1,5 +0,0 @@
-vex::brain Brain;
-vex::motor RR = vex::motor(vex::PORT15);
-vex::motor LR = vex::motor(vex::PORT19);
-vex::motor RF = vex::motor(vex::PORT16);
-vex::motor LF = vex::motor(vex::PORT18);
